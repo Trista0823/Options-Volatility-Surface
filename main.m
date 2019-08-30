ee4cd99cca63e5cdedd2bf88b7a874380d038ce3
Trista0823 <49023184+Trista0823@users.noreply.github.com>
2019-08-29 21:47:43 -0400

    Update and rename Untitled.m to main.m

--- main.m
+++ main.m
@@ -2,9 +2,15 @@ clear
 warning off
 data = csvread('50etf.csv',1,0); %date s shibor
 data(:,3) = data(:,3)/100;
-data_put = csvread('put.csv',1,0); %date strike p left (s shibor)
-data_call = csvread('call.csv',1,0); %date strike c left (s shibor)
-data_put(data_put(:,4)<=1,:) = [];          %消除调仓效应
+data_put = csvread('put.csv',1,0); 
+%date strike p left (s shibor)
+
+data_call = csvread('call.csv',1,0); 
+%date strike c left (s shibor)
+
+data_put(data_put(:,4)<=1,:) = [];         
+%eliminate the effect of adjusting positions
+
 data_call(data_call(:,4)<=1,:) = [];
 data_put(:,4) = data_put(:,4)/365;
 data_call(:,4) = data_call(:,4)/365;
@@ -23,6 +29,8 @@ for i = 1:length(data)
         end
     end
 end
+
+% calculate implied volatility of call options
 x = zeros(length(data_call),2);
 v_call = zeros(length(data_call),3);  %std maturity moneyness
 ff=optimset('display','off');
@@ -32,9 +40,6 @@ for i = 1:length(data_call)
     K = data_call(i,2);
     T = data_call(i,4);
     c = data_call(i,3);
-    %d1 = (log(S/K)+r*T)/(x*T^0.5) + 0.5*x*T^0.5;
-    %d2 = (log(S/K)+r*T)/(x*T^0.5) + 0.5*x*T^0.5 - x*T^0.5;
-    %yy = inline('S*normcdf((log(S/K)+r*T)/(x*T^0.5) + 0.5*x*T^0.5) - K*exp(-r*T)*normcdf((log(S/K)+r*T)/(x*T^0.5) + 0.5*x*T^0.5 - x*T^0.5) - c');
     x(i,:) = fsolve(@(x)BS_call(x,S,r,K,T,c),[0,1],ff);
     v_call(i,:) = [x(i,2),T,(S-K)/K];
 end
@@ -42,8 +47,7 @@ call_y = v_call(:,2);
 call_x = v_call(:,3);
 call_z = v_call(:,1);
 
-
-
+% calculate implied volatility of put options
 v_put = zeros(length(data_put),3);  %std maturity moneyness
 for i = 1:length(data_put)
     S = data_put(i,5);
@@ -51,9 +55,6 @@ for i = 1:length(data_put)
     K = data_put(i,2);
     T = data_put(i,4);
     c = data_put(i,3);
-    %d1 = (log(S/K)+r*T)/(x*T^0.5) + 0.5*x*T^0.5;
-    %d2 = (log(S/K)+r*T)/(x*T^0.5) + 0.5*x*T^0.5 - x*T^0.5;
-    %yy = inline('S*normcdf((log(S/K)+r*T)/(x*T^0.5) + 0.5*x*T^0.5) - K*exp(-r*T)*normcdf((log(S/K)+r*T)/(x*T^0.5) + 0.5*x*T^0.5 - x*T^0.5) - c');
     x(i,:) = fsolve(@(x)BS_put(x,S,r,K,T,c),[0,1],ff);
     v_put(i,:) = [x(i,2),T,(K-S)/K];
 end
